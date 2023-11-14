e2f26ca9307b920c0d851d2aa8ee2068b247cd7f
melekabbassi <abbassimelek@gmail.com>
2023-11-14 20:45:30 +0100

    primary starwars clone cpmplete, add api next

--- lib/screens/MovieDetails/moviedetails.dart
+++ lib/screens/MovieDetails/moviedetails.dart
@@ -12,10 +12,171 @@ class _MovieDetailsState extends State<MovieDetails> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text("Think-it Star Wars")),
+        title: const Center(child: Text("A New Hope")),
       ),
-      body: const Center(
-        child: Text("Movie Details"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 500,
+            height: 200,
+            color: Colors.black,
+            child: const Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "A New Hope",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Release date",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            "1977-05-25",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: 14,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Director",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            "George Lucas",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: 14,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Producer",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            "Gary Kurtz, Rick McCallum",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("Opening Crawl",
+                    style: TextStyle(
+                      fontSize: 12,
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                child: Text(
+                    "It is a period of civil war.\r Rebel spaceships, striking\rfrom a hidden base, have won\rtheir first victory against\rthe evil Galactic Empire.\rDuring the battle, Rebel\rspies managed to steal secret\rplans to the Empire's\rultimate weapon, the DEATH\rSTAR, an armored space\rstation with enough power\rto destroy an entire planet.\r\rPursued by the Empire's\rsinister agents, Princess\rLeia races home aboard her\rstarship, custodian of the\rstolen plans that can save her\rpeople and restore\rfreedom to the galaxy....",
+                    style: TextStyle(
+                      fontSize: 12,
+                    )),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Created",
+                            style: TextStyle(
+                              fontSize: 12,
+                            )),
+                        Text("2014/2/10",
+                            style: TextStyle(
+                              fontSize: 12,
+                            ))
+                      ],
+                    ),
+                    SizedBox(
+                      width: 14,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Edited",
+                            style: TextStyle(
+                              fontSize: 12,
+                            )),
+                        Text("2017/2/10",
+                            style: TextStyle(
+                              fontSize: 12,
+                            ))
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
